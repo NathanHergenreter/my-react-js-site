09e56fec3554d99ba49b33117ed66d55d7139b55
NathanHergenreter <nathanhergenreter@gmail.com>
2023-12-26 14:43:48 -0600

    Added Home component, added bootstrap

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,19 @@
+function Home() {
+    return (
+    <div className="container">
+        <div className="row">
+            <h1>Hello World!</h1>
+        </div>
+        <div className="row">
+            <div className="col-6">
+                <p>This is a col-6</p>
+            </div>
+            <div className="col-6">
+                <p>This is another col-6</p>
+            </div>
+        </div>
+    </div>
+    );
+}
+
+export default Home;
