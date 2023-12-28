7477d898e7d873e4e84d715c29757ea7b152555d
NathanHergenreter <megalibasileis@gmail.com>
2023-12-27 20:13:29 -0600

    Added navbar component

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import Home from './Components/Home/Home';
 import reportWebVitals from './reportWebVitals';
+import Home from './Components/Home/Home';
+import Navbar from './Components/Navbar/Navbar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Navbar />
     <Home />
   </React.StrictMode>
 );
