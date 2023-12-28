40dfdd66735f95ca1a8048ee8f4e4cee8b847a47
NathanHergenreter <megalibasileis@gmail.com>
2023-12-27 20:56:37 -0600

    Container padding, messed around with home page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,15 +1,12 @@
 function Home() {
     return (
-    <div className="container">
-        <div className="row">
-            <h1>Hello World!</h1>
-        </div>
-        <div className="row">
-            <div className="col-6">
-                <p>This is a col-6</p>
+    <div className="container-fluid">
+            <div className="container">
+                <h1>Hello World!</h1>
             </div>
-            <div className="col-6">
-                <p>This is another col-6</p>
+        <div className="bg-light">
+            <div className="container">
+                <p>This is a col-6</p>
             </div>
         </div>
     </div>
