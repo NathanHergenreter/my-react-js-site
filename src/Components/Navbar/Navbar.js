5f06293cbcb784c7e944623127620b637047056f
NathanHergenreter <megalibasileis@gmail.com>
2024-01-04 20:22:24 -0600

    Hover styling for navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,5 +1,7 @@
 import { NavLink } from "react-router-dom";
 
+import './Navbar.css';
+
 function Navbar() {
     return (
     <nav className="navbar navbar-expand-lg py-3">
@@ -14,17 +16,6 @@ function Navbar() {
                     </li>
                     <li className="nav-item">
                         <NavLink className="nav-link" to="/fun-things">Fun Things</NavLink>
-                    </li>
-                    <li className="nav-item dropdown">
-                        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Dropdown
-                        </a>
-                        <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a className="dropdown-item" href="#">Action</a></li>
-                            <li><a className="dropdown-item" href="#">Another action</a></li>
-                            <li><hr className="dropdown-divider"></hr></li>
-                            <li><a className="dropdown-item" href="#">Something else here</a></li>
-                        </ul>
                     </li>
                 </ul>
             </div>
