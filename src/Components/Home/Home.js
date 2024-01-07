6f4a1df88a44039dc33dce895fe91b5da1351c39
NathanHergenreter <megalibasileis@gmail.com>
2024-01-07 13:24:05 -0600

    Started work on actual home page content

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,16 +1,28 @@
+import placeholderPicture from '../../Assets/Images/avatar-default.svg';
+
+import './Home.css';
+
 function Home() {
     return (
-    <div className="bg-main">
-        <div className="container">
-            <h1>Hello World!</h1>
-        </div>
-        <div className="bg-alt">
-            <div className="container">
-                <p>This is my website. It looks kinda bad right now but that's okay :)</p>
+    <div className='bg-alt'>
+        <div className='container'>
+            <div className='row'>
+                <div className='col-6 align-self-center mb-5'>
+                    <h1 className='hello-title'>Hello World</h1>
+                    <h5 className='py-3'>
+                        Welcome to my ReactJs website! Here, you can see my progress in learning ReactJs from scratch and learn some facts about me.
+                    </h5>
+                    <p>
+                        Feel free to browse through the site and check out whatever piques your interest! See my <a href='/resume'>Resume</a> page for more info on me and my background.
+                    </p>
+                    <p>
+                        I've also made some fun components to play with, go ahead and check them out if you have time!
+                    </p>
+                </div>
+                <div className='col-6 text-center'>
+                    <img className='placeholder-avatar' src={placeholderPicture} alt='Me'></img>
+                </div>
             </div>
-        </div>
-        <div className="container">
-            <p>Feel free to click on any of the links in the navbar to see some of the stuff I've made (it's not that good either).</p>
         </div>
     </div>
     );
