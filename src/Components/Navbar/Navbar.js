603a805c060e21b0a4ce05c0447b566380a5f095
NathanHergenreter <megalibasileis@gmail.com>
2024-01-07 13:24:21 -0600

    Navbar styling, added Resume page link

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,25 +1,14 @@
-import { NavLink } from "react-router-dom";
+import { NavLink } from 'react-router-dom';
 
 import './Navbar.css';
 
 function Navbar() {
     return (
-    <nav className="navbar navbar-expand-lg py-3">
-        <div className="container-fluid">
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li className="nav-item">
-                        <NavLink className="nav-link active" aria-current="page" to="/">Home</NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink className="nav-link" to="/blogs">Blogs</NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink className="nav-link" to="/fun-things">Fun Things</NavLink>
-                    </li>
-                </ul>
-            </div>
-        </div>
+    <nav className='nav navbar navbar-expand-lg py-3 justify-content-center bg-main'>
+        <NavLink className='nav-link' aria-current='page' to='/'>Home</NavLink>
+        <NavLink className='nav-link' to='/resume'>Resume</NavLink>
+        <NavLink className='nav-link' to='/blogs'>Blogs</NavLink>
+        <NavLink className='nav-link' to='/fun-things'>Fun Things</NavLink>
     </nav>
     );
 }
